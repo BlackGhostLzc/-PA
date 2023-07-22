7f86c82ada514af0223418eac8d8f280c53afb4d
tracer-ics2022 <tracer@njuics.org>
2023-07-22 11:16:05 +0800

    >  compile NEMU 202166191459 刘钊铖 Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:16:05 up  2:08,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -52,7 +52,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
   if (check_watchpoint(&point))
   {
     printf("Stoped at \e[1;36mWatchPoint(NO.%d)\e[0m: %s \n", point->NO, point->condation);
-    puts(_this->logbuf);
+    // puts(_this->logbuf);
     nemu_state.state = NEMU_STOP;
   }
 }
