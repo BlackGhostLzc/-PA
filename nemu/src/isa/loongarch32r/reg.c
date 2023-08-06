a23d21de1def5d56cdef4aa1b4bae1bae71cc0e3
tracer-ics2022 <tracer@njuics.org>
2023-08-06 22:08:38 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:08:38 up 13:12,  0 users,  load average: 0.18, 0.15, 0.15

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -24,8 +24,30 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  int length = ARRLEN(regs);
+  int i = 0;
+  printf("=========寄存器信息=========\n");
+  for (i = 0; i < length; i+= REGISTERS_PER_LINE){
+    for (int j = i; j < MIN(length, i + REGISTERS_PER_LINE); ++j){
+      printf("\e[1;36m%3s:\e[0m %#12x | ", regs[j], cpu.gpr[j]._32);
+    }
+    printf("\n");
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+  *success = true;
+  if (strcmp(s, regs[0]) == 0){
+    return cpu.gpr[0]._32;
+  }
+
+  for (int i = 1; i < ARRLEN(regs); ++i){
+    if (strcmp(regs[i], s+1) == 0){//跳过$
+      *success = true;
+      return cpu.gpr[i]._32;
+    }
+  }
+
+  *success = false;
+  return -1;
 }
