f958b1e38830c3993382345e832a169b9f36ccd4
tracer-ics2022 <tracer@njuics.org>
2023-09-20 15:46:11 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:46:11 up 32 min,  0 users,  load average: 0.26, 0.14, 0.09

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -1,48 +1,55 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 #include "local-include/reg.h"
 
 const char *regs[] = {
-  "$0", "ra", "tp", "sp", "a0", "a1", "a2", "a3",
-  "a4", "a5", "a6", "a7", "t0", "t1", "t2", "t3",
-  "t4", "t5", "t6", "t7", "t8", "rs", "fp", "s0",
-  "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"
-};
+    "$0", "ra", "tp", "sp", "a0", "a1", "a2", "a3",
+    "a4", "a5", "a6", "a7", "t0", "t1", "t2", "t3",
+    "t4", "t5", "t6", "t7", "t8", "rs", "fp", "s0",
+    "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"};
 
-void isa_reg_display() {
+void isa_reg_display()
+{
+  assert(0);
   int length = ARRLEN(regs);
   int i = 0;
   printf("=========寄存器信息=========\n");
-  for (i = 0; i < length; i+= REGISTERS_PER_LINE){
-    for (int j = i; j < MIN(length, i + REGISTERS_PER_LINE); ++j){
+  for (i = 0; i < length; i += REGISTERS_PER_LINE)
+  {
+    for (int j = i; j < MIN(length, i + REGISTERS_PER_LINE); ++j)
+    {
       printf("\e[1;36m%3s:\e[0m %#12x | ", regs[j], cpu.gpr[j]._32);
     }
     printf("\n");
   }
 }
 
-word_t isa_reg_str2val(const char *s, bool *success) {
+word_t isa_reg_str2val(const char *s, bool *success)
+{
   *success = true;
-  if (strcmp(s, regs[0]) == 0){
+  if (strcmp(s, regs[0]) == 0)
+  {
     return cpu.gpr[0]._32;
   }
 
-  for (int i = 1; i < ARRLEN(regs); ++i){
-    if (strcmp(regs[i], s+1) == 0){//跳过$
+  for (int i = 1; i < ARRLEN(regs); ++i)
+  {
+    if (strcmp(regs[i], s + 1) == 0)
+    { // 跳过$
       *success = true;
       return cpu.gpr[i]._32;
     }
