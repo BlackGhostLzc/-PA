e5c36d80460426d585d7f27ea220c882c0959e06
tracer-ics2022 <tracer@njuics.org>
2023-09-25 20:06:52 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:06:52 up  7:53,  0 users,  load average: 0.10, 0.13, 0.10

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,41 +5,58 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-int rand(void) {
+int rand(void)
+{
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
+void srand(unsigned int seed)
+{
   next = seed;
 }
 
-int abs(int x) {
+int abs(int x)
+{
   return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr)
+{
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
+  while (*nptr == ' ')
+  {
+    nptr++;
+  }
+  while (*nptr >= '0' && *nptr <= '9')
+  {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
 
-void *malloc(size_t size) {
+void *malloc(size_t size)
+{
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  // panic("Not implemented");
+  if (size % 4)
+  {
+    size = (size / 4 + 1) * 4;
+  }
+  void *ret = heap.end;
+  heap.end += size;
+  return ret;
 #endif
   return NULL;
 }
 
-void free(void *ptr) {
+void free(void *ptr)
+{
 }
 
 #endif
