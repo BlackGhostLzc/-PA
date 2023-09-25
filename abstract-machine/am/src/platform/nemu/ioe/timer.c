4e0b2f8243eed5f0faf9912939f9c2e7ce779bf0
tracer-ics2022 <tracer@njuics.org>
2023-09-25 18:44:55 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:44:55 up  6:31,  0 users,  load average: 0.02, 0.10, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,7 +7,11 @@ void __am_timer_init()
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
-  uptime->us += 1;
+  // AM_DEVREG(6, TIMER_UPTIME, RD, uint64_t us) 会创建一个结构体 TIMER_UPTIME_T,它有一个成员 uint64_t us
+  // 我们需要从设备寄存器中读取时间，然后把这个时间写会这个 TIMER_UPTIME_T 物理内存中的变量
+  uint64_t low = inl(RTC_ADDR);
+  uint64_t high = inl(RTC_ADDR + 4);
+  uptime->us = low + (high << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc)
