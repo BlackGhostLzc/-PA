fad916facdb3be0a5f06ec79a86661bf2482cffb
tracer-ics2022 <tracer@njuics.org>
2023-09-26 15:45:33 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:45:33 up  1:29,  0 users,  load average: 0.12, 0.16, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -17,9 +17,9 @@ void __am_gpu_init()
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 {
-  uint32_t data = inl(VGACTL_ADDR);
-  int w = (data >> 16);
-  int h = (data & 0xffff);
+  uint32_t screen_wh = inl(VGACTL_ADDR);
+  uint32_t h = screen_wh & 0xffff;
+  uint32_t w = screen_wh >> 16;
   *cfg = (AM_GPU_CONFIG_T){
       .present = true, .has_accel = false, .width = w, .height = h, .vmemsz = 0};
 }
@@ -28,18 +28,19 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 {
   // ctl int x, y; void *pixels; int w, h; bool sync
   // 要把这些内容写入显存中 uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR
-  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0))
+    return;
+  uint32_t *pixels = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t *p = ctl->pixels;
-
-  for (int y = ctl->y; y < ctl->y + ctl->h; y++)
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y + h; i++)
   {
-    for (int x = ctl->x; x < ctl->x + ctl->w; x++)
+    for (int j = x; j < x + w; j++)
     {
-      fb[y * screen_w + x] = p[(y - ctl->y) * screen_w + x - ctl->x];
+      fb[screen_w * i + j] = pixels[w * (i - y) + (j - x)];
     }
   }
-
   if (ctl->sync)
   {
     outl(SYNC_ADDR, 1);
