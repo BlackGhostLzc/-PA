2eb98dd8b39a05534655db7340e6ab3a992d6fc4
tracer-ics2022 <tracer@njuics.org>
2023-09-26 09:09:09 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:09:09 up 31 min,  0 users,  load average: 0.29, 0.11, 0.08

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,7 +3,12 @@
 
 #define KEYDOWN_MASK 0x8000
 
-void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
+{
+  // bool keydown; int keycode
+  // kbd->keydown = 0;
+  // kbd->keycode = AM_KEY_NONE;
+  uint32_t data = inl(KBD_ADDR);
+  kbd->keydown = (bool)(data & KEYDOWN_MASK);
+  kbd->keycode = (data & (~KEYDOWN_MASK));
 }
