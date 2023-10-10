28d67ff7b8268c1e3a326e00b417d96160db11ba
tracer-ics2022 <tracer@njuics.org>
2023-10-10 09:48:38 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:48:38 up  1:15,  0 users,  load average: 0.19, 0.06, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,21 +2,41 @@
 #include <elf.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // check valid elf
+  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr) * ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++)
+  {
+    if (phdr[i].p_type == PT_LOAD)
+    {
+      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      // set .bss with zeros
+      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+  }
+  return ehdr.e_entry;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename)
+{
+  // 这是一个批处理程序，没有实现并发
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
-
