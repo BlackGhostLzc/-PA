a03af10b19818d7011ce0c141db34237224e897e
tracer-ics2022 <tracer@njuics.org>
2023-10-10 09:38:43 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:38:43 up  1:05,  0 users,  load average: 0.08, 0.04, 0.00

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -6,28 +6,35 @@ static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
 
-void switch_boot_pcb() {
+void switch_boot_pcb()
+{
   current = &pcb_boot;
 }
 
-void hello_fun(void *arg) {
+void hello_fun(void *arg)
+{
   int j = 1;
-  while (1) {
+  while (1)
+  {
     Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
-    j ++;
+    j++;
     yield();
   }
 }
 
-void init_proc() {
+void naive_uload(PCB *pcb, const char *filename);
+
+void init_proc()
+{
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-
+  naive_uload(NULL, NULL);
 }
 
-Context* schedule(Context *prev) {
+Context *schedule(Context *prev)
+{
   return NULL;
 }
