f653cf36b2df8ecf6351c82742c0153663d3d490
tracer-ics2022 <tracer@njuics.org>
2023-10-11 10:29:33 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:29:33 up  1:08,  0 users,  load average: 0.57, 0.23, 0.12

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,24 @@
 #include <common.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
+void do_syscall(Context *c)
+{
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
-  switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  switch (a[0])
+  {
+
+  case SYS_exit:
+    break;
+  case SYS_yield:
+    yield();
+    c->GPRx = 0;
+    break;
+
+  default:
+    panic("Unhandled syscall ID = %d", a[0]);
   }
 }
