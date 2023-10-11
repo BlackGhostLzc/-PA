8cb0a49b24f1e32a0cd9485062590a028525ed9c
tracer-ics2022 <tracer@njuics.org>
2023-10-11 16:52:55 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:52:55 up 41 min,  0 users,  load average: 0.40, 0.23, 0.15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,22 @@
 #include <common.h>
 #include "syscall.h"
+/*
+static char *syscall_names[] = {
+    "SYS_exit", "SYS_yield", "SYS_open", "SYS_read", "SYS_write", "SYS_kill", "SYS_getpid", "SYS_close",
+    "SYS_lseek", "SYS_brk", "SYS_fstat", "SYS_time", "SYS_signal", "SYS_execve", "SYS_fork", "SYS_link",
+    "SYS_unlink", "SYS_wait", "SYS_times", "SYS_gettimeofday"};
+*/
 void do_syscall(Context *c)
 {
   uintptr_t a[4];
-  a[0] = c->GPR1;
-  a[1] = c->GPR2;
-  a[2] = c->GPR3;
+  a[0] = c->GPR1; // a7
+  a[1] = c->GPR2; // a0
+  a[2] = c->GPR3; // a1
   a[3] = c->GPR4;
+
+#ifdef CONFIG_STRACE
+  printf("STRACE: [#%3ld]( %ld, %ld, %ld )\n", a[0], a[1], a[2], a[3]);
+#endif
 
   switch (a[0])
   {
@@ -18,8 +28,21 @@ void do_syscall(Context *c)
     yield();
     c->GPRx = 0;
     break;
-
+  case SYS_write:
+    if (a[1] == 1 || a[1] == 2)
+    {
+      for (int i = 0; i < a[3]; i++)
+      {
+        putch(*((char *)(a[2] + i)));
+      }
+    }
+    c->GPRx = a[3];
+    break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
+
+#ifdef CONFIG_ITRACE
+  printf("SYSCALL:       [%s] -> %ld\n", syscall_names[a[0]], c->GPRx);
+#endif
 }
