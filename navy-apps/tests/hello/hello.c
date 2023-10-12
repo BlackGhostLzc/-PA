709e1db7cae63468499382881af8847a32cdc52a
tracer-ics2022 <tracer@njuics.org>
2023-10-12 14:36:27 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:36:27 up 23 min,  0 users,  load average: 0.10, 0.13, 0.11

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,8 +4,8 @@
 int main()
 {
   write(1, "Hello World!\n", 13);
-  int i = 2;
-  volatile int j = 0;
+  // int i = 2;
+  // volatile int j = 0;
   /*
   while (1) {
     j ++;
@@ -15,6 +15,6 @@ int main()
     }
   }
   */
-  printf("Hello World from Navy-apps for the first time!\n");
+  // printf("Hello World from Navy-apps for the first time!\n");
   return 0;
 }
