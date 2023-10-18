2afcd8a964df79c5667f39693fdbef0272abb51d
tracer-ics2022 <tracer@njuics.org>
2023-10-18 14:17:40 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:17:40 up  4:59,  0 users,  load average: 0.29, 0.25, 0.25

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -36,7 +36,18 @@ void do_syscall(Context *c)
     c->GPRx = 0;
     break;
   case SYS_write:
-    c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
+    /*
+      c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
+      break;
+    */
+    if (a[1] == 1 || a[1] == 2)
+    {
+      for (int i = 0; i < a[3]; i++)
+      {
+        putch(*((char *)(a[2] + i)));
+      }
+    }
+    c->GPRx = a[3];
     break;
 
   case SYS_brk:
