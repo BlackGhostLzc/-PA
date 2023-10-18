e8231a027864233dc21b4b6e498cfc8a1c19753e
tracer-ics2022 <tracer@njuics.org>
2023-10-18 16:38:22 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:38:22 up  7:20,  0 users,  load average: 0.24, 0.13, 0.19

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,9 +15,10 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t serial_write(const void *buf, size_t offset, size_t len)
 {
+  char *str = (char *)buf;
   for (int i = 0; i < len; ++i)
   {
-    putch(*((char *)buf + i));
+    putch(str[i]);
   }
   return len;
 }
