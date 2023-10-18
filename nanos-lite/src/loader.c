eddffb6e0e2afca488a9ef622c32d872e1a7aa3f
tracer-ics2022 <tracer@njuics.org>
2023-10-18 16:21:35 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:21:35 up  7:03,  0 users,  load average: 0.31, 0.40, 0.38

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -21,12 +21,6 @@ size_t fs_lseek(int fd, size_t offset, int whence);
 
 int fs_open(const char *pathname, int flags, int mode);
 
-static void read(int fd, void *buf, size_t offset, size_t len)
-{
-  fs_lseek(fd, offset, SEEK_SET);
-  fs_read(fd, buf, len);
-}
-
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
   // 这部分只在 PA3 第二阶段需要，那个阶段的ramdisk.img被替换为hello或是dummy程序的二进制代码，
@@ -56,7 +50,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     SEEK_CUR,
     SEEK_END
   };
-  /*
+
   int fd = fs_open(filename, 0, 0);
   assert(fd >= 0);
 
@@ -81,46 +75,12 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     }
   }
   return ehdr.e_entry;
-  */
-
-  int fd = fs_open(filename, 0, 0);
-
-  Elf_Ehdr elf_header;
-  read(fd, &elf_header, 0, sizeof(elf_header));
-  // 根据小端法 0x7F E L F
-  assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
-
-  size_t program_header_offset = elf_header.e_phoff;
-  size_t headers_entry_size = elf_header.e_phentsize;
-  int headers_entry_num = elf_header.e_phnum;
-
-  for (int i = 0; i < headers_entry_num; ++i)
-  {
-    Elf_Phdr section_entry;
-    read(fd, &section_entry,
-         i * headers_entry_size + program_header_offset, sizeof(elf_header));
-    void *virt_addr;
-    switch (section_entry.p_type)
-    {
-    case PT_LOAD:
-      virt_addr = (void *)section_entry.p_vaddr;
-      read(fd, virt_addr, section_entry.p_offset, section_entry.p_filesz);
-      memset(virt_addr + section_entry.p_filesz, 0,
-             section_entry.p_memsz - section_entry.p_filesz);
-      break;
-
-    default:
-      break;
-    }
-  }
-
-  return elf_header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename)
 {
   // 这是一个批处理程序，没有实现并发  文件在 navy-apps/fsimg/bin中
-  uintptr_t entry = loader(pcb, "/bin/file-test");
+  uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
   ((void (*)())entry)();
 }
