00f98e92827da0f0bf7f7671f79c6b80e40853d5
tracer-ics2022 <tracer@njuics.org>
2023-10-19 16:30:48 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:30:48 up  5:40,  0 users,  load average: 0.50, 0.33, 0.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,9 +4,11 @@
 #ifdef __LP64__
 #define Elf_Ehdr Elf64_Ehdr
 #define Elf_Phdr Elf64_Phdr
+#define ELF_Off Elf64_Off
 #else
 #define Elf_Ehdr Elf32_Ehdr
 #define Elf_Phdr Elf32_Phdr
+#define ELF_Off Elf32_Off
 #endif
 enum
 {
@@ -20,6 +22,12 @@ size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 
 int fs_open(const char *pathname, int flags, int mode);
+
+static void read(int fd, void *buf, size_t offset, size_t len)
+{
+  fs_lseek(fd, offset, SEEK_SET);
+  fs_read(fd, buf, len);
+}
 
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
@@ -51,34 +59,67 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     SEEK_CUR,
     SEEK_END
   };
+  /*
+    int fd = fs_open(filename, 0, 0);
+    assert(fd >= 0);
+
+    Elf_Ehdr ehdr;
+    size_t len = fs_read(fd, (void *)&ehdr, sizeof(ehdr));
+    assert(len == sizeof(ehdr));
+    assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+
+    Elf_Phdr phdr[ehdr.e_phnum];
+    fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
+    fs_read(fd, (void *)phdr, ehdr.e_phentsize * ehdr.e_phnum);
+
+    for (int i = 0; i < ehdr.e_phnum; i++)
+    {
+      if (phdr[i].p_type == PT_LOAD)
+      {
+        // Elf_Phdr:程序头表包含了可执行文件在内存中的各个段的信息
+        fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+        fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_memsz);
+        // .bss 要填 0
+        memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+      }
+    }
+    printf("hahahahah\n");
+    // assert(0);
+    return ehdr.e_entry;
+  */
 
   int fd = fs_open(filename, 0, 0);
-  assert(fd >= 0);
 
-  Elf_Ehdr ehdr;
-  size_t len = fs_read(fd, (void *)&ehdr, sizeof(ehdr));
-  assert(len == sizeof(ehdr));
-  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+  Elf_Ehdr elf_header;
+  read(fd, &elf_header, 0, sizeof(elf_header));
+  // 根据小端法 0x7F E L F
+  assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
 
-  Elf_Phdr phdr[ehdr.e_phnum];
-  fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
-  fs_read(fd, (void *)phdr, ehdr.e_phentsize * ehdr.e_phnum);
+  ELF_Off program_header_offset = elf_header.e_phoff;
+  size_t headers_entry_size = elf_header.e_phentsize;
+  int headers_entry_num = elf_header.e_phnum;
 
-  for (int i = 0; i < ehdr.e_phnum; i++)
+  for (int i = 0; i < headers_entry_num; ++i)
   {
-    if (phdr[i].p_type == PT_LOAD)
+    Elf_Phdr section_entry;
+    read(fd, &section_entry,
+         i * headers_entry_size + program_header_offset, sizeof(elf_header));
+    void *virt_addr;
+    switch (section_entry.p_type)
     {
-      // Elf_Phdr:程序头表包含了可执行文件在内存中的各个段的信息
-      fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
-      fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_memsz);
-      // .bss 要填 0
-      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    case PT_LOAD:
+      virt_addr = (void *)section_entry.p_vaddr;
+      read(fd, virt_addr, section_entry.p_offset, section_entry.p_filesz);
+      memset(virt_addr + section_entry.p_filesz, 0,
+             section_entry.p_memsz - section_entry.p_filesz);
+      break;
+
+    default:
+      break;
     }
   }
-  printf("hahahahah\n");
-  // assert(0);
-  return ehdr.e_entry;
 
+  return elf_header.e_entry;
   /*
     int fd = fs_open(filename, 0, 0);
 
