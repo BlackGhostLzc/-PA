2cf1a06a097b0b297524ed149b55331a59c9d5d7
tracer-ics2022 <tracer@njuics.org>
2023-10-19 12:30:20 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:30:20 up  1:39,  0 users,  load average: 0.38, 0.30, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -34,8 +34,9 @@ void do_syscall(Context *c)
   switch (a[0])
   {
   case SYS_exit:
-    halt(a[1]);
-    c->GPR4 = a[1];
+    // halt(a[1]);
+    // c->GPR4 = a[1];
+    c->GPRx = 0;
     break;
   case SYS_yield:
     yield();
