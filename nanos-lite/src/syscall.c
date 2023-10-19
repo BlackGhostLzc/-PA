7b97342ad50be3a2f9dfb81c606d5bb8b68c6d54
tracer-ics2022 <tracer@njuics.org>
2023-10-19 14:21:53 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:21:53 up  3:31,  0 users,  load average: 0.21, 0.13, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,6 +13,7 @@ size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
 
+/*
 void sys_write(Context *c)
 {
   int ret = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4);
@@ -47,7 +48,7 @@ void do_syscall(Context *c)
     sys_write(c);
     break;
 
-    /*
+
     if (a[1] == 1 || a[1] == 2)
     {
       for (int i = 0; i < a[3]; i++)
@@ -58,7 +59,7 @@ void do_syscall(Context *c)
     c->GPRx = a[3];
     break;
     */
-
+/*
   case SYS_brk:
     c->GPRx = 0;
     break;
@@ -81,8 +82,114 @@ void do_syscall(Context *c)
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
+*/
+// #ifdef CONFIG_SYSCALLTRACE
+//  printf("SYSCALL:       [%s] -> %ld\n", syscall_names[a[0]], c->GPRx);
+// #endif
+// }
 
-  // #ifdef CONFIG_SYSCALLTRACE
-  //  printf("SYSCALL:       [%s] -> %ld\n", syscall_names[a[0]], c->GPRx);
-  // #endif
+void sys_yield(Context *c)
+{
+  // yield();
+  c->GPRx = 0;
+}
+
+void sys_exit(Context *c)
+{
+  c->GPRx = 0;
+}
+void sys_write(Context *c)
+{
+  // if (c->GPR2 == 1 || c->GPR2 == 2){
+  //   for (int i = 0; i < c->GPR4; ++i){
+  //     putch(*(((char *)c->GPR3) + i));
+  //   }
+  //   c->GPRx = c->GPR4;
+  // }else {
+  int ret = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4);
+  c->GPRx = ret;
+  // }
+}
+
+void sys_read(Context *c)
+{
+  int ret = fs_read(c->GPR2, (void *)c->GPR3, c->GPR4);
+  c->GPRx = ret;
+}
+
+void sys_brk(Context *c)
+{
+  c->GPRx = 0;
+}
+
+void sys_open(Context *c)
+{
+  int ret = fs_open((char *)c->GPR2, c->GPR3, c->GPR4);
+  c->GPRx = ret;
+}
+
+void sys_close(Context *c)
+{
+  int ret = fs_close(c->GPR2);
+  c->GPRx = ret;
+}
+
+void sys_lseek(Context *c)
+{
+  int ret = fs_lseek(c->GPR2, c->GPR3, c->GPR4);
+  c->GPRx = ret;
+}
+
+// struct __temp{
+//  #if __SIZEOF_POINTER__ == 8
+//   __uint64_t sec;
+//   __uint64_t usec;
+//  #elif __SIZEOF_POINTER__ == 4
+//   __uint32_t sec;
+//   __uint32_t usec;
+// #endif
+// };
+
+void do_syscall(Context *c)
+{
+  uintptr_t a[4];
+  a[0] = c->GPR1;
+
+  switch (a[0])
+  {
+  case SYS_yield:
+    sys_yield(c);
+    break;
+
+  case SYS_exit:
+    sys_exit(c);
+    break;
+
+  case SYS_write:
+    sys_write(c);
+    break;
+
+  case SYS_brk:
+    sys_brk(c);
+    break;
+
+  case SYS_open:
+    sys_open(c);
+    break;
+
+  case SYS_close:
+    sys_close(c);
+    break;
+
+  case SYS_read:
+    sys_read(c);
+    break;
+
+  case SYS_lseek:
+    sys_lseek(c);
+    break;
+
+  default:
+    panic("Unhandled syscall ID = %d", a[0]);
+  }
 }
