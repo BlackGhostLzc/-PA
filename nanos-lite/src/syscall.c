6885bb0efddd41772e774f7dc89a4dd2341d7c02
tracer-ics2022 <tracer@njuics.org>
2023-10-19 15:13:37 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:13:37 up  4:23,  0 users,  load average: 0.37, 0.25, 0.17

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -155,7 +155,10 @@ void do_syscall(Context *c)
 {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPRx;
+  printf("SYSCALLTRACE: [#%3ld]( %ld, %ld, %ld )\n", a[0], a[1], a[2], a[3]);
   switch (a[0])
   {
   case SYS_yield:
