78866489d14b47d3555a986e2d1e9c5faabebbbe
tracer-ics2022 <tracer@njuics.org>
2023-10-19 21:16:17 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:16:17 up  2:01,  0 users,  load average: 0.42, 0.29, 0.15

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -99,20 +99,20 @@ void *_sbrk(intptr_t increment)
 
 int _read(int fd, void *buf, size_t count)
 {
-  _exit(SYS_read);
-  return 0;
+  int ret = _syscall_(SYS_read, fd, (intptr_t)(buf), count);
+  return ret;
 }
 
 int _close(int fd)
 {
-  _exit(SYS_close);
-  return 0;
+  int ret = _syscall_(SYS_read, 0, 0, 0);
+  return ret;
 }
 
 off_t _lseek(int fd, off_t offset, int whence)
 {
-  _exit(SYS_lseek);
-  return 0;
+  int ret = _syscall_(SYS_lseek, fd, offset, whence);
+  return ret;
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz)
