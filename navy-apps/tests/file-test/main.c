3c34a4b21fe96cf1fce8880ec12ce71a6aa06be7
tracer-ics2022 <tracer@njuics.org>
2023-10-19 21:10:57 +0800

    >  compile NEMU 202166191459 BlackGhost Linux LAPTOP-6BH4BGNM 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:10:57 up  1:56,  0 users,  load average: 0.25, 0.19, 0.09

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -6,24 +6,21 @@ int main()
   // assert(0);
   // printf("here0\n");
   FILE *fp = fopen("/share/files/num", "r+");
-  printf("here3\n");
-  assert(0);
   assert(fp);
 
-  assert(0);
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-  assert(0);
+
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
-  assert(0);
+
   for (i = 500; i < 1000; i++)
   {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  assert(0);
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i++)
   {
@@ -42,9 +39,9 @@ int main()
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
-  assert(0);
+
   fclose(fp);
-  assert(0);
+
   printf("PASS!!!\n");
 
   return 0;
